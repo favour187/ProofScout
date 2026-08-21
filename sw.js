deb687eefd769c0163d5d33b3bc29cba744a310c
favour187 <favouronilamo@gmail.com>
2026-08-21 10:32:52 +0000

    odds and ends

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='proofscout-v17';
-const ASSETS=['./','./index.html','./privacy.html','./proofscout-offline.html','./demo-target.html','./styles.css','./app.js','./scout.js','./scout.css','./manifest.webmanifest','./icon.svg'];
+const CACHE='proofscout-v18';
+const ASSETS=['./','./index.html','./privacy.html','./proofscout-offline.html','./demo-target.html','./styles.css','./app.js','./scout.js','./scout.css','./manifest.webmanifest','./icon.svg','./favicon.ico','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
